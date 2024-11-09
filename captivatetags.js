d7676f244d8f9502ab6fc907721bee7ac56f88c5
Xavi <xavi@xavimt.com>
2024-11-09 14:26:21 +0100

    Create exclude.css

---
+++ captivatetags.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tagElements = document.querySelectorAll(".tag-1");
+
+    tagElements.forEach((tagElement) => {
+        const tag = tagElement.querySelector(".tag");
+        const tagText = tag.textContent;
+
+        tagElement.setAttribute("data-category", "captivate.tags");  // Set data-category by default
+        
+        tagElement.addEventListener("click", function () {
+            tagElement.classList.toggle("active");
+            tag.classList.toggle("active");  // Toggle active class on tag
+
+            // Toggle data-id attribute based on the "active" class
+            if (tagElement.classList.contains("active")) {
+                tagElement.setAttribute("data-id", tagText);
+            } else {
+                tagElement.removeAttribute("data-id");
+            }
+        });
+    });
+});
